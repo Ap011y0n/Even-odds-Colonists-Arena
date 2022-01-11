4f5975ea506930c051af5d8b342458a982c2047a
Ap011y0n <47557365+Ap011y0n@users.noreply.github.com>
2022-01-11 08:32:49 +0100

    cursor lock

---
+++ Online Videogame/Assets/scripts/UiManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UiManager : MonoBehaviour
+{
+    private bool menuOpen = false;
+    public GameObject UI_Menu;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(menuOpen)
+            {
+                menuOpen = false;
+                UI_Menu.SetActive(menuOpen);
+                Cursor.lockState = CursorLockMode.Locked;
+            }
+            else
+            {
+                menuOpen = true;
+                UI_Menu.SetActive(menuOpen);
+                Cursor.lockState = CursorLockMode.None;
+            }
+        }
+    }
+}
